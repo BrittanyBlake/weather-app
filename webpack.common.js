b71c092130aba2892fc46d1a8c393229db0f4c9b
Brittany Blake <brittany.blake49@gmail.com>
2020-09-08 17:43:11 +0200

    add webpack

---
+++ webpack.common.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+const path = require("path");
+/* eslint-enable */
+
+module.exports = {
+  entry: {
+    main: "./src/index.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs",
+          },
+        },
+      },
+    ],
+  },
+};
