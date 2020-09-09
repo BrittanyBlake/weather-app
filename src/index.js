c358268981712ea14622fed46aa7a5c2b42ac0d0
Brittany Blake <brittany.blake49@gmail.com>
2020-09-09 12:33:00 +0200

    add form element

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles/style.scss';
+import form from './js/search-form'
+
+
+const container = document.querySelector('#container')
+container.appendChild(form)
---
+++ src/js/search-form.js
@@ -0,0 +1,24 @@
+ const form = (() => {
+   const nav = document.createElement("nav");
+   const form = document.createElement("form");
+   form.setAttribute("class", "form")
+   const input = document.createElement("input");
+   input.setAttribute("type", "text");
+   input.setAttribute("class", "search");
+   input.setAttribute("placeholder", "Enter a city");
+    const button = document.createElement("button");
+    button.setAttribute("class", "button");
+    button.setAttribute("type", "submit");
+    button.textContent = "SEARCH";
+
+    
+   form.appendChild(input);
+   form.appendChild(button);
+   nav.appendChild(form);
+   return nav;
+ })();
+
+
+ export default form;
+
+
