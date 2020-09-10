0f3033049a25fcf69f80b5addf9f66fbe221758d
Brittany Blake <brittany.blake49@gmail.com>
2020-09-10 02:47:38 +0200

    add icons for weather

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import footer from "./js/footer";
 
 const Skycons = require("skycons")(window);
 
+const skycon = new Skycons({ color: "white" });
+
 const container = document.querySelector("#container");
 container.appendChild(form);
 container.appendChild(main);
@@ -33,6 +35,7 @@ async function getWeather(value) {
   const data = result;
   displayData(data);
   changeImage(data);
+  console.log(data);
 }
 
 getWeather("granada");
@@ -52,6 +55,53 @@ const displayData = (data) => {
   humidity.innerHTML = `Humidity: ${data.main.humidity}%`;
   smallText.innerHTML = `${data.weather[0].description}`;
   convertTemp(data);
+  addSkycon(data);
+};
+
+const addSkycon = (data) => {
+  if (data.weather[0].main === "Clouds") {
+    skycon.set("icon", "cloudy");
+    skycon.play();
+  } else if (
+    data.weather[0].main === "Rain" ||
+    data.weather[0].main === "Thunderstorms" ||
+    data.weather[0].main === "Drizzle"
+  ) {
+    skycon.set("icon", "rain");
+    skycon.play();
+  } else if (
+    data.weather[0].main === "Mist" ||
+    data.weather[0].main === "Smoke" ||
+    data.weather[0].main === "Haze" ||
+    data.weather[0].main === "Dust" ||
+    data.weather[0].main === "Fog" ||
+    data.weather[0].main === "Sand" ||
+    data.weather[0].main === "Ash" ||
+    data.weather[0].main === "Squall"
+  ) {
+    skycon.set("icon", "fog");
+    skycon.play();
+  } else if (data.weather[0].icon === "01n") {
+    skycon.set("icon", "clear_night");
+    skycon.play();
+  } else if (data.weather[0].icon === "01d") {
+    skycon.set("icon", "clear_day");
+    skycon.play();
+  } else if (
+    data.weather[0].icon === "Wind" ||
+    data.weather[0].main === "Tornado"
+  ) {
+    skycon.set("icon", "wind");
+    skycon.play();
+  } else if (
+    data.weather[0].main === "Snow"
+  ) {
+    skycon.set("icon", "snow");
+    skycon.play();
+  } else {
+    skycon.set("icon", "clear_day");
+    skycon.play();
+  }
 };
 
 const changeImage = (data) => {
